9bea51c5dd52652f4fa6793098623140f31867a6
mbm777 <balamuralim111@gmail.com>
2025-09-01 14:56:03 +0530

    conlict resolved

--- src/main/java/java8/filter/TVSeries.java
+++ src/main/java/java8/filter/TVSeries.java
@@ -1,1 +1,22 @@
+package java8.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TVSeries {
+	private String name;
+	private String runtime_of_series;
+	private String certificate;
+	private String runtime_of_episodes;
+	private String genre;
+	private double imdb_rating;
+	private String overview;
+	private long no_of_votes;
+	private int id;
+	
+}
 
